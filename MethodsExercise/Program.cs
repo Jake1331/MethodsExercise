e3fb0cb25c876c711c82ffae701a2b56cb79f078
Jake1331 <michaeldruummer@gmail.com>
2024-05-04 01:34:14 -0500

    Completed the exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace MethodsExercise
+﻿using System;
+
+namespace MethodsExercise
 {
     public class Program
     {
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   Console.WriteLine("What is your name ?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color ?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal ?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite kind of music ?");
+            var music = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}, your favorite color is {color}, your favorite animal is {animal}, and your favorite kind of music is {music}.");
         }
     }
 }
+
