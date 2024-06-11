af1b454aba417c783f61bad08e644a374853e9c0
Jake1331 <michaeldruummer@gmail.com>
2024-06-10 21:41:07 -0500

    Completed the methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,11 +2,32 @@
 
 namespace MethodsExercise
 {
+
+
     public class Program
     {
 
+
         static void Main(string[] args)
-        {   Console.WriteLine("What is your name ?");
+        {
+            ShortStory();
+
+            int added = Add(5,3);
+            Console.WriteLine(added);
+
+            int subtracted = Subtract(5,3);
+            Console.WriteLine(subtracted);
+
+            int muliplied = Multiply(3,5);
+            Console.WriteLine(muliplied);
+
+            int divide = Divide(10,5);
+            Console.WriteLine(divide);
+        }
+
+        public static void ShortStory() 
+        { 
+         Console.WriteLine("What is your name ?");
             var name = Console.ReadLine();
 
             Console.WriteLine("What is your favorite color ?");
@@ -20,6 +41,26 @@ namespace MethodsExercise
 
             Console.WriteLine($"Your name is {name}, your favorite color is {color}, your favorite animal is {animal}, and your favorite kind of music is {music}.");
         }
-    }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        { 
+            return num1 - num2; 
+        }
+
+        public static int Multiply(int num1, int num2) 
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        { 
+            return num1 / num2; 
+        }
+    } 
 }
 
